49c740a4a7ae180cfed885967df98e09ca7fa396
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-14 21:30:13 +0530

    conversion celsius to Fahrenheit

---
+++ project4.scala
@@ -0,0 +1,11 @@
+object project4 {
+
+  def main(args: Array[String]) : Unit = {
+
+
+    val temp=35;
+    val farah=temp * 1.800 + 32.00;
+    println("fahrenheit of 35c is " + farah + "f");
+  }
+
+}
